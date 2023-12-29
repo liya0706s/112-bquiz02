e76b278c58c13067dae624881aa06d71cce57b35
Angie <liya0706s@gmail.com>
2023-12-29 11:24:48 +0800

    建立頁面跟註冊登入相關的頁面

--- index.php
+++ index.php
@@ -1,48 +1,71 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!-- saved from url=(0039) -->
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+﻿<?php include_once "./api/db.php"; ?>
 
-<title>健康促進網</title>
-<link href="./css/css.css" rel="stylesheet" type="text/css">
-<script src="./js/jquery-1.9.1.min.js"></script>
-<script src="./js/js.js"></script>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0039) -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	<title>健康促進網</title>
+	<link href="./css/css.css" rel="stylesheet" type="text/css">
+	<script src="./js/jquery-1.9.1.min.js"></script>
+	<script src="./js/js.js"></script>
 </head>
 
 <body>
-<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
-	<pre id="ssaa"></pre>
-</div>
-<iframe name="back" style="display:none;"></iframe>
+	<div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
+		<pre id="ssaa"></pre>
+	</div>
 	<div id="all">
-    	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
-        <div id="title2">
-        	
-        </div>
-        <div id="mm">
-        	<div class="hal" id="lef">
-            	                	    <a class="blo" href="?do=po">分類網誌</a>
-               	                     	    <a class="blo" href="?do=news">最新文章</a>
-               	                     	    <a class="blo" href="?do=pop">人氣文章</a>
-               	                     	    <a class="blo" href="?do=know">講座訊息</a>
-               	                     	    <a class="blo" href="?do=que">問卷調查</a>
-               	                 </div>
-            <div class="hal" id="main">
-            	<div>
-            		
-                	<span style="width:18%; display:inline-block;">
-                    	                    	<a href="?do=login">會員登入</a>
-                    	                    </span>
-                    	<div class="">
-                		                        </div>
-                </div>
-            </div>
-        </div>
-        <div id="bottom">
-    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
-    		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
-        </div>
-    </div>
+		<div id="title">
+			<!-- 今日日期的total，是今日瀏覽次數 -->
+			<!-- sum() 參數要有欄位是total -->
+			<?= date("m月d日 l"); ?>| 今日瀏覽:<?= $Total->find(['date' => date("Y-m-d")])['total']; ?> | 累積瀏覽: <?= $Total->sum('total'); ?>
+		</div>
+		<a href="index.php" style="float:right">回首頁</a>
+		<div id="title2" title='健康促進網-回首頁'>
+			<img src="./icon/02B01.jpg" alt="">
+		</div>
+		<div id="mm">
+			<div class="hal" id="lef">
+				<a class="blo" href="?do=po">分類網誌</a>
+				<a class="blo" href="?do=news">最新文章</a>
+				<a class="blo" href="?do=pop">人氣文章</a>
+				<a class="blo" href="?do=know">講座訊息</a>
+				<a class="blo" href="?do=que">問卷調查</a>
+			</div>
+			<div class="hal" id="main">
+				<div>
+					<!-- 會員登入上方 放跑馬燈 -->
+					<!-- marquee是block, 會把會員登入擠下去, 要加上style inline-block -->
+					<marquee style="width:78%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
+					<span style="width:21%; display:inline-block;">
+						<a href="?do=login" style="float:right">會員登入</a>
+					</span>
+					<div class="">
 
-</body></html>
+						<?php
+						$do = $_GET['do'] ?? 'main';
+						$file="./front/{$do}.php";
+						if(file_exits($file)){
+							include $file;
+						}else{
+							
+						}
+						?>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="bottom">
+			本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
+			<br>
+			服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+		</div>
+	</div>
+
+</body>
+
+</html>
