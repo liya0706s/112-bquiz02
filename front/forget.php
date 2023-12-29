fafb2ba79071b42db8c4a9dcec465e1ffa16f603
Angie <liya0706s@gmail.com>
2023-12-29 13:09:03 +0800

    建置忘記密碼頁面

---
+++ front/forget.php
@@ -0,0 +1,9 @@
+<fieldset>
+    <legend>忘記密碼</legend>
+    <div>請輸入信箱以查詢密碼</div>
+    <div><input type="text" name="" id=""></div>
+    <div id="result"></div>
+    <div>
+        <input type="button" value="尋找">
+    </div>
+</fieldset>
