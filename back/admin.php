f5304f2b2c85c0b26cd2a83997e887e8c917af09
Angie <liya0706s@gmail.com>
2024-01-05 11:11:51 +0800

    隱藏admin帳號不顯示出來

--- back/admin.php
+++ back/admin.php
@@ -4,31 +4,33 @@
     </legend>
 
     <form action="./api/edit_user.php" method="post">
-    <table style="width:55%;margin:auto">
-        <tr>
-            <!-- 內建css底色灰色clo -->
-            <td class="clo">帳號</td>
-            <td class="clo">密碼</td>
-            <td class="clo">刪除</td>
-        </tr>
-
-        <?php
-        $rows = $User->all();
-        foreach ($rows as $row) {
-        ?>
+        <table style="width:55%;margin:auto">
             <tr>
-                <td><?= $row['acc']; ?></td>
-                <td><?= str_repeat("*", mb_strlen($row['pw'])); ?></td>
-                <td><input type="checkbox" name="del[]" value="<?= $row['id']; ?>"></td>
+                <!-- 內建css底色灰色clo -->
+                <td class="clo">帳號</td>
+                <td class="clo">密碼</td>
+                <td class="clo">刪除</td>
             </tr>
-        <?php
-        }
-        ?>
-    </table>
-    <div class="ct">
-        <input type="submit" value="確定刪除">
-        <input type="reset" value="清空選取">
-    </div>
+
+            <?php
+            $rows = $User->all();
+            foreach ($rows as $row) {
+                if ($row['acc'] != 'admin') {
+            ?>
+                    <tr>
+                        <td><?= $row['acc']; ?></td>
+                        <td><?= str_repeat("*", mb_strlen($row['pw'])); ?></td>
+                        <td><input type="checkbox" name="del[]" value="<?= $row['id']; ?>"></td>
+                    </tr>
+            <?php
+                }
+            }
+            ?>
+        </table>
+        <div class="ct">
+            <input type="submit" value="確定刪除">
+            <input type="reset" value="清空選取">
+        </div>
     </form>
 
     <h1>新增會員</h1>
